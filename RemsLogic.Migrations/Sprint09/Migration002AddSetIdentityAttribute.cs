73c694ca7bee394e6c478830c67f09da354c3914
mlindegarde <mlindegarde@pivotalhorizon.com>
2014-04-24 06:26:43 -0400

    Adding the files I forgot to add.

---
+++ RemsLogic.Migrations/Sprint09/Migration002AddSetIdentityAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FluentMigrator;
+
+namespace RemsLogic.Migrations.Sprint09
+{
+    [Migration(201404240519)]
+    public class Migration002AddSetIdentityAttribute : Migration
+    {
+        public override void Up()
+        {
+            Delete.Table("UserEocsLog");
+
+            Create.Table("UserEocsLog")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("UserEocsId").AsInt64().NotNullable()
+                .WithColumn("RecordedAt").AsDateTime().NotNullable();
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
