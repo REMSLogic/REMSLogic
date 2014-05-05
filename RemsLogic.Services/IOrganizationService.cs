dbbb2b5a5e6317d24ab9c1374a8fe4117d93109a
mlindegarde <mlindegarde@pivotalhorizon.com>
2014-05-05 12:40:47 -0400

    added organization service.

---
+++ RemsLogic.Services/IOrganizationService.cs
@@ -0,0 +1,9 @@
+﻿using RemsLogic.Model;
+
+namespace RemsLogic.Services
+{
+    public interface IOrganizationService
+    {
+        void Save(Organization organization);
+    }
+}
---
+++ RemsLogic.Services/OrganizationService.cs
@@ -0,0 +1,27 @@
+﻿using RemsLogic.Model;
+using RemsLogic.Repositories;
+
+namespace RemsLogic.Services
+{
+    public class OrganizationService : IOrganizationService
+    {
+        #region Member Variables
+        private readonly IOrganizationRepository _orgRepo;
+        #endregion
+
+        #region Constructors
+        public OrganizationService(
+            IOrganizationRepository orgRepo)
+        {
+            _orgRepo = orgRepo;
+        }
+        #endregion
+
+        #region IOrganizationRepository Implementation
+        public void Save(Organization organization)
+        {
+            _orgRepo.Save(organization);
+        }
+        #endregion
+    }
+}
