68f49f2912f95832e0421e1a58cf84c8ca57f1a7
mlindegarde <mlindegarde@pivotalhorizon.com>
2014-07-15 07:34:22 -0400

    Added migration for LinkId.

---
+++ RemsLogic.Migrations/Sprint16/Migration0003AddLinkIdToUserEocs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentMigrator;
+
+namespace RemsLogic.Migrations.Sprint16
+{
+    [Migration(201407150733)]
+    public class Migration0003AddLinkIdToUserEocs : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("UserEocs")
+                .AddColumn("LinkId").AsInt64().NotNullable().WithDefaultValue(0);
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
