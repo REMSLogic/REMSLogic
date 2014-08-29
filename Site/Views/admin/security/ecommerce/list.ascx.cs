767b2b2d7212b80c388f41c38eaae5fb336945eb
mlindegarde <mlindegarde@pivotalhorizon.com>
2014-08-29 13:21:23 -0400

    Added class to provider.  Added menu item to admin menu.  Added list page for e-commerce users.

---
+++ Site/Views/admin/security/ecommerce/list.ascx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Lib.Data;
+
+namespace Site.Views.admin.security.ecommerce
+{
+    public partial class list : Lib.Web.AppControlPage
+    {
+        public IList<ProviderUser> ProviderUsers {get; set;}
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ProviderUsers = Lib.Data.ProviderUser.FindByClass();
+        }
+    }
+}
