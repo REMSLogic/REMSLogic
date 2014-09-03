6d698cc3520a3e6e95f12242e92f3c9e3e472c95
mlindegarde <mlindegarde@pivotalhorizon.com>
2014-09-03 01:09:02 -0400

    Added migrations.

---
+++ RemsLogic.Migrations/Sprint26/Migration0001AddClassToProviderUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentMigrator;
+
+namespace RemsLogic.Migrations.Sprint26
+{
+    [Migration(201409020103)]
+    public class Migration0001AddClassToProviderUser : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("ProviderUsers")
+                .AddColumn("Class").AsString(16).WithDefaultValue("standard");
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
