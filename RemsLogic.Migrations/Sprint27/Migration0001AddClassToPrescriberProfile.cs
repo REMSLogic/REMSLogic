a254b4657065abfa743bdad8495aa27ed2cf882a
mlindegarde <mlindegarde@pivotalhorizon.com>
2014-09-06 13:00:11 -0400

    Addeed prescriber profile migrator.

---
+++ RemsLogic.Migrations/Sprint27/Migration0001AddClassToPrescriberProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentMigrator;
+
+namespace RemsLogic.Migrations.Sprint27
+{
+    [Migration(201409061258)]
+    public class Migration0001AddClassToPrescriberProfile : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("PrescriberProfiles")
+                .AddColumn("Class").AsString(16).WithDefaultValue("standard");
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
